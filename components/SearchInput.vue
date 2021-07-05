bfd5658001bef15d0a678bf21a394c5fbe0fcba2
kris <nov.kri@yandex.ru>
2021-07-06 01:17:48 +0700

    init

---
+++ components/SearchInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="handleSearch">
+    <div class="uk-inline uk-width-1-2">
+      <a class="uk-form-icon uk-form-icon-flip" href="#" uk-icon="icon: chevron-right; ratio: 1.5" @click="handleSearch"></a>
+      <!-- <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6"/></svg>-->
+      <input type="text" v-model="whatToSearch" class="uk-input search-input" placeholder="Search for any IP address or domain" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'search-input',
+  // props: {
+  //   whatToSearch: String
+  // }
+  data() {
+    return {
+      whatToSearch: ''
+    }
+  },
+
+  methods: {
+    handleSearch() {
+      console.log(this.whatToSearch)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-input {
+  padding: 30px;
+  border-radius: 15px;
+  border: none;
+  outline: none;
+}
+
+.uk-form-icon {
+  background-color: #000;
+  border-radius: 0 15px 15px 0;
+  width: 60px;
+}
+
+</style>
---
+++ views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <header class="home_header">IP Address Tracker</header>
+    <SearchInput />
+    <
+  </div>
+</template>
+
+<script>
+
+import SearchInput from "@/components/SearchInput";
+export default {
+  name: "Home",
+  components: {
+    SearchInput,
+  },
+
+};
+</script>
+
+<style>
+.home {
+  background-image: url("~@/assets/images/pattern-bg.png");
+  background-position: top center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 250px;
+}
+
+.home_header {
+  color: #fff;
+  font-size: 26px;
+  font-weight: bold;
+  padding-top: 30px;
+  padding-bottom: 25px;
+}
+</style>
